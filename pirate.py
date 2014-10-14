8d367b330e0bcfdc66b219f3ab17a45ac1b6fb52
Tatiana Tylosky <tatianatylosky@gmail.com>
2014-10-14 19:44:08 -0400

    after code review

--- pirate.py
+++ pirate.py
@@ -17,41 +17,25 @@ ingredients = {
     "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
 }
 
-strong = ""
-salty = ""
-bitter = ""
-sweet = ""
-fruity = ""
-
 def ask():
-    global strong
-    strong = raw_input(questions["strong"] + " (y/n)")
-    global salty
-    salty = raw_input(questions["salty"] + " (y/n)")
-    global bitter
-    bitter = raw_input(questions["bitter"] + " (y/n)")
-    global sweet
-    sweet = raw_input(questions["sweet"] + " (y/n)")
-    global fruity
-    fruity = raw_input(questions["fruity"] + " (y/n)")
+    response = {}
+    for key in questions:
+        response[key] = raw_input(questions[key] + " (y/n)")
+    return response
+
+def make_drink(response):
+    for key in response:
+        if response[key] == "y":
+            print("just a " + random.choice(ingredients[key]))
 
 ready = raw_input('Are you ready to make your pirate drink? (y/n) ')
 while ready != 'y':
     ready = raw_input('Are you ready to make your pirate drink? (y/n) ')
     print "Argsome!"
 else:
-    ask()
+    response = ask()
 
 print "I'll make that right up for you!"
 print "..."
-if strong == "y":
-    print ("just a " + random.choice(ingredients["strong"]))
-if salty == "y":
-    print ("a bit of " + random.choice(ingredients["salty"]))
-if bitter == "y":
-    print ("a good portion of " + random.choice(ingredients["bitter"]))
-if sweet == "y":
-    print ("and just one " + random.choice(ingredients["sweet"]))
-if fruity == "y":
-    print ("garnished with a " + random.choice(ingredients["fruity"]))
+make_drink(response)
 print "Voila!"
